b9094bc8032a812a7dbc67013a9acb5ae1923c5f
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-02 22:23:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -128,6 +128,7 @@ function retry() {
 	data.score = 0;
 	data.currentWord = words[Math.floor(Math.random()*words.length)];
 	document.getElementById("word").style = `filter: blur(0);`
+	document.getElementById("yourspelling").value = "";
 }
 function update() {
 	document.getElementById("score").innerText = `Score: ${data.score}`
